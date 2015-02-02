9b5c619c416282e7560798252d538cd79c3d8143
Will Jones <willjones@ua.edu>
2015-02-02 15:33:23 -0600

    Added the default settings file to the repo

---
+++ default-roots-child-settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * RENAME: remove the 'default-' from the file name to activate.
+ *
+ * Use this file to define custom variables and globals for the roots-ualib theme.
+ *
+ * Be sure to change the default values of the definitions below.
+ */
+
+// Replace with absolute file path to the webapps folder
+define('WEBAPPS_PATH', "/path/to/webapps");
+
+// Replace with the host URL - prefixed with '//' and appended with '/'
+define('ROOTS_UALIB_HOST', "//localhost/");
